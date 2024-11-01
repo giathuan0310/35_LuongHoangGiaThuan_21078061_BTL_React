104136e8999250001a7aa72e7884bf3e0b94965e
giathuan0310 <luonggiathuan2019.gr@gmail.com>
2024-11-01 16:18:17 +0700

    Cai dat thu vien va tao ra screen chuyen trang

---
+++ screens/Screen_02.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+
+export default function Screen_02({navigation}){
+    return(
+       <Text>Helloo</Text>
+    );
+}
+

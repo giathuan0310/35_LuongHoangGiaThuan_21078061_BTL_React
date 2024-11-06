3dd5d2524d1eed63f137fc5064fcca4f72ac0c06
giathuan0310 <luonggiathuan2019.gr@gmail.com>
2024-11-07 04:58:22 +0700

    hoan thanh xong giao dien step 2 tien trinh thanh toan

--- screens/TravellerInformation.js
+++ screens/TravellerInformation.js
@@ -4,6 +4,7 @@ import { View, Text, FlatList, Image, Picker,StyleSheet, ScrollView, TouchableOp
 import { Ionicons } from '@expo/vector-icons';
 //npm i react-native-progress-steps cài tiến trinh
 import { ProgressSteps, ProgressStep } from 'react-native-progress-steps';
+import { CheckBox } from 'react-native-web';
 const TravellerInformation = () => {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
@@ -13,10 +14,26 @@ const TravellerInformation = () => {
     const [phone, setPhone] = useState('+07');   
   
     //tien trinh
-    const[step1Data,setStep1Data]=useState({name:'',address:''});
-    const[step2Data,setStep2Data]=useState({email:'',username:''});
-    const[step3Data,setStep3Data]=useState({password:'',retypePassword:''});
+    // const[step1Data,setStep1Data]=useState({name:'',address:''});
+    // const[step2Data,setStep2Data]=useState({email:'',username:''});
+    // const[step3Data,setStep3Data]=useState({password:'',retypePassword:''});
     
+     //Step 2
+    // Trạng thái cho các mục checkbox, sử dụng một object để lưu trữ các mục đã chọn
+    const [selectedOptions, setSelectedOptions] = useState({
+        personalItem: false,
+        checkedBag: false,
+        noCheckedBag: false,
+        protect: false,
+        noInsurance: false,
+    });
+
+    const handleCheckboxPress = (option) => {
+        setSelectedOptions((prevOptions) => ({
+            ...prevOptions,
+            [option]: !prevOptions[option], // Chuyển đổi trạng thái của mục đã chọn
+        }));
+    };
     return (
         <SafeAreaView style={styles.safeArea}>
         <View style={styles.container}>
@@ -29,9 +46,10 @@ const TravellerInformation = () => {
                             {/* Khu vực có thể cuộn cho nội dung toàn bộ màn hình */}
                             <ScrollView style={{ width: "100%", height: 600 }}>  
                            <ProgressSteps>
-                             
-                                <ProgressStep label='Step 1'>
+                                {/* Step1 */}
+                                <ProgressStep label={<Ionicons name="person" size={24} color="black" style={styles.backIcon} />}>
                                 <Text style={styles.title}>Traveller information</Text>
+                                <View style={styles.hr}/>
                                         {/* Phần  body*/}
                                         <View style={styles.bodyiInput}>
                                                         <Text >Traveller: 1 adult</Text>
@@ -91,29 +109,95 @@ const TravellerInformation = () => {
                                                     
                                         </View>
                                         {/* footer */}
-             <View style={styles.buttonContainer}>
-                    <Text style={styles.price}>$806</Text> 
-                </View>
-     
-
-                                            
-
+                                        <View style={styles.buttonContainer}>
+                                                <Text style={styles.price}>$806</Text> 
+                                        </View>
                                 </ProgressStep>
 
-                                    <ProgressStep label='Step 2'>
-                                        <View style={styles.stepContent}>
-                                                
+                                {/* Step2 */}
+                                <ProgressStep label={<Ionicons name="briefcase" size={24} color="black" style={styles.backIcon} />}>
+                                    <Text style={styles.title}>Baggage</Text>
+                                    <View style={styles.hr}/>
+                                    <View style={styles.bodyInput}>
+                                        <View style={styles.containInput}>
+                                            <label style={styles.textLabel}>Cabin bags</label>
+                                            <TouchableOpacity onPress={() => handleCheckboxPress("personalItem")} style={styles.checkboxContainer}>
+                                                <View style={{ flexDirection: 'row' }}>
+                                                    <Ionicons name="briefcase" size={20} color="black" />
+                                                    <Text style={styles.checkboxText}>Personal item only</Text>
+                                                    <View style={styles.checkbox}>
+                                                        {selectedOptions.personalItem && <Ionicons name="radio-button-on" size={15} color="#00BDD5" />}
+                                                    </View>
+                                                </View>
+                                                <Text style={styles.checkboxSubtext}>Included per traveller</Text>
+                                            </TouchableOpacity>
+                                            
+                                            <View style={styles.hr}/>
+                                            <label style={styles.textLabel}>Checked bags</label>
+                                            <TouchableOpacity onPress={() => handleCheckboxPress("checkedBag")} style={styles.checkboxContainer}>
+                                                <View style={{ flexDirection: 'row' }}>
+                                                    <Ionicons name="briefcase" size={20} color="black" />
+                                                    <Text style={styles.checkboxText}>1 checked bag (Max weight 22.1 lbs)</Text>
+                                                    <View style={styles.checkbox}>
+                                                        {selectedOptions.checkedBag && <Ionicons name="radio-button-on" size={15} color="#00BDD5" />}
+                                                    </View>
+                                                </View>
+                                                <Text style={styles.checkboxSubtext}>from $19.99</Text>
+                                            </TouchableOpacity>
+
+                                            <TouchableOpacity onPress={() => handleCheckboxPress("noCheckedBag")} style={styles.checkboxContainer}>
+                                                <View style={{ flexDirection: 'row' }}>
+                                                    <Ionicons name="remove-circle" size={20} color="black" />
+                                                    <Text style={styles.checkboxText}>No checked bags</Text>
+                                                    <View style={styles.checkbox}>
+                                                        {selectedOptions.noCheckedBag && <Ionicons name="radio-button-on" size={15} color="#00BDD5" />}
+                                                    </View>
+                                                </View>
+                                                <Text style={styles.checkboxSubtext}>$00.00</Text>
+                                            </TouchableOpacity>
+
+                                            <View style={styles.hr}/>
+                                            <label style={styles.textLabel}>Travel protection</label>
+                                            <TouchableOpacity onPress={() => handleCheckboxPress("protect")} style={styles.checkboxContainer}>
+                                                <View style={{ flexDirection: 'row' }}>
+                                                    <Ionicons name="shield" size={20} color="black" />
+                                                    <Text style={styles.checkboxText}>Travel protection</Text>
+                                                    <View style={styles.checkbox}>
+                                                        {selectedOptions.protect && <Ionicons name="radio-button-on" size={15} color="#00BDD5" />}
+                                                    </View>
+                                                </View>
+                                                <Text style={styles.checkboxSubtext}>from $19.99</Text>
+                                            </TouchableOpacity>
+
+                                            <Image source={require('../assets/imageProtect.png')} style={styles.ImageProtect} />
+
+                                            <TouchableOpacity onPress={() => handleCheckboxPress("noInsurance")} style={styles.checkboxContainer}>
+                                                <View style={{ flexDirection: 'row' }}>
+                                                    <Ionicons name="remove-circle" size={20} color="black" />
+                                                    <Text style={styles.checkboxText}>No insurance</Text>
+                                                    <View style={styles.checkbox}>
+                                                        {selectedOptions.noInsurance && <Ionicons name="radio-button-on" size={15} color="#00BDD5" />}
+                                                    </View>
+                                                </View>
+                                                <Text style={styles.checkboxSubtext}>$00.00</Text>
+                                            </TouchableOpacity>
                                         </View>
+                                    </View>
+                                    <View style={styles.buttonContainer}>
+                                        <Text style={styles.price}>$806</Text> 
+                                    </View>
+                                </ProgressStep>
+                                {/* Step3 */}
+                                <ProgressStep label={<Ionicons name="bed" size={24} color="black" style={styles.backIcon} />}>
+                                       
 
-                                    </ProgressStep>
-
-                                    <ProgressStep label='Step 3'>
-                                        <View style={styles.stepContent}>
-                                              
-                                        </View>
-
-                                    </ProgressStep>
+                                </ProgressStep>
                            
+                                {/* Step4 */}
+                                <ProgressStep label={<Ionicons name="card" size={24} color="black" style={styles.backIcon} />}>
+                                        
+
+                                </ProgressStep>
                            </ProgressSteps>
                            </ScrollView>
                            
@@ -161,7 +245,7 @@ const TravellerInformation = () => {
       fontSize: 20,
       fontWeight: 'bold',
       marginBottom: 10,
-      marginLeft:80
+      textAlign:'center'
     },
     bodyiInput:{
         padding: 20,
@@ -176,7 +260,7 @@ const TravellerInformation = () => {
       
     },
     textLabel:{
-        
+      
         fontWeight: 'bold', 
 
     },
@@ -201,15 +285,48 @@ const TravellerInformation = () => {
 
 
     },
-//css cho progess
-stepContent:{
+    hr: {
+        width: '100%',
+        height: 1,
+        backgroundColor: '#ccc',
+        marginVertical: 20,
+      },
 
+    checkboxContainer: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginBottom: 10,
 },
-labelText:{
-
+checkbox: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    marginRight: 8, // Adjusted spacing between checkbox and text
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    
 },
+checkboxText: {
+    fontSize: 16,
+    marginLeft: 10, // Add space to the left of the text
+},
+checkboxSubtext: {
+    fontSize: 12,
+    color: 'gray',
+},
+ImageProtect:{
+    width:300,
+    height:300,
+    borderRadius:5,
+    borderColor:'gray'
 
+}
 
-  });
+    });
+
+  
   
   export default TravellerInformation;
